67513f7814a938b3fbdbe81debb05006dd2d33aa
Znamm9 <v.rudenko106@gmail.com>
2022-04-29 12:21:51 +0300

    first commit

---
+++ src/test/java/Tests.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+public class Tests {
+
+    @Test(groups = {"nums"})
+    public void test1() {
+    }
+
+    @Test
+    public void test2() {
+    }
+
+    @Test
+    public void test3() {
+    }
+
+    @Test
+    public void test4() {
+    }
+}
